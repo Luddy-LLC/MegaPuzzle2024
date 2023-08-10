988fcca46fc798bd1f3557971b1bb613c984270d
Matei Cloteaux <matei.cloteaux@gmail.com>
2023-08-10 12:35:31 -0400

    Map Drag (Fixed)

--- scripts/mapDrag.js
+++ scripts/mapDrag.js
@@ -1,70 +1,48 @@
-// Make the DIV element draggable:
+const ele = document.getElementById("mapContainer");
+ele.scrollTop = 100;
+ele.scrollLeft = 150;
 
-const map = document.getElementById("draggableMap");
+dragElement(ele);
 
+function dragElement(elmnt) {
 
+  let pos = {xi : 0, yi : 0, xf : 0, yf : 0};
 
-dragElement();
-let maxTop = map.getBoundingClientRect().height - 20 - window.innerHeight;
-let maxLeft = map.getBoundingClientRect().width - 20 - window.innerWidth;
-let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-
-function dragElement() {
-
-  map.onmouseleave = closeDragElement;
-  document.onmouseleave = closeDragElement;
-
-  map.onmousedown = dragMouseDown;
+  elmnt.onmousedown = dragMouseDown;
+  
 
   function dragMouseDown(e) {
     e.preventDefault();
+
     // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    map.onmouseup = closeDragElement;
+    pos.xf = e.clientX;
+    pos.yf = e.clientY;
+    document.onmouseup = closeDragElement;
     // call a function whenever the cursor moves:
-    map.onmousemove = elementDrag;
+    document.onmousemove = elementDrag;
+
+    console.log("Mouse down");
   }
 
   function elementDrag(e) {
-
     e.preventDefault();
     // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
+
+    pos.xi = pos.xf;
+    pos.yi = pos.yf;
+    pos.xf = e.clientX;
+    pos.yf = e.clientY;
+
     // set the element's new position:
-    map.style.top = Math.max(-maxTop, Math.min(0,(map.offsetTop - pos2))) + "px";
-    map.style.left = Math.max(-maxLeft, Math.min(0,(map.offsetLeft - pos1))) + "px";
+    elmnt.scrollLeft = (elmnt.scrollLeft - (pos.xf - pos.xi));
+    elmnt.scrollTop = (elmnt.scrollTop - (pos.yf - pos.yi));
+
+    console.log("mouse drage");
   }
 
   function closeDragElement() {
     // stop moving when mouse button is released:
-    map.onmouseup = null;
-    map.onmousemove = null;
+    document.onmouseup = null;
+    document.onmousemove = null;
   }
-}
-
-window.addEventListener("keydown", e => {
-
-  if(e.shiftKey)
-    e.preventDefault();
-
-  if(e.ctrlKey && (e.key == "=" || e.key == "-")){
-    console.log("zooming");
-    e.preventDefault();
-  }
-});
-
-window.addEventListener("resize", e => {
-  maxTop = map.getBoundingClientRect().height - 20 - window.innerHeight;
-  maxLeft = map.getBoundingClientRect().width - 20 - window.innerWidth;
-
-  map.style.top = Math.max(-maxTop, Math.min(0,(map.offsetTop - pos2))) + "px";
-  map.style.left = Math.max(-maxLeft, Math.min(0,(map.offsetLeft - pos1))) + "px";
-});
-
-function addMapDragability(){
-  
 }
