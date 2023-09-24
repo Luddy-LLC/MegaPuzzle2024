b03d339035f20eb7506b32ac333e5facd924739b
Calvin Josenhans <60663079+HalflingHelper@users.noreply.github.com>
2023-09-24 15:05:36 -0400

    Adding Assets to Snake

--- Minigames/snake/game.js
+++ Minigames/snake/game.js
@@ -10,17 +10,25 @@
 const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
 
-const boardSize = 15;
-let cellSize = 32;
-let apple = [13, 13];
-let snake = [[0, 2], [0, 1], [0, 0]];
-let snakeDir = [0, 1]
-let snakeDirections = {
+const appleImg = document.getElementById("apple");
+const snakeHead = document.getElementById("snake-head");
+const snakeBody = document.getElementById("snake-body");
+const snakeCorner = document.getElementById("snake-corner");
+const snakeTail = document.getElementById("snake-tail");
+
+
+const snakeDirections = {
     "left": [-1, 0],
     "right": [1, 0],
     "up": [0, -1],
     "down": [0, 1]
 }
+const boardSize = 16;
+let cellSize = 32;
+let apple = [13, 13];
+let snake = [[0, 2], [0, 1], [0, 0]];
+let snakeDir = snakeDirections.right;
+
 let isDead = false;
 let directionQueue = []
 
@@ -28,10 +36,17 @@ let clueInterval = 5
 let clue = ["Y", "R", "A", "N", "I", "D", "R", "O"]
 let cluedClues = 0
 
+function arrayEquals(a, b) {
+    return Array.isArray(a) &&
+        Array.isArray(b) &&
+        a.length === b.length &&
+        a.every((val, index) => val === b[index]);
+}
+
 function resetGame() {
     apple = [13, 13];
     snake = [[0, 2], [0, 1], [0, 0]];
-    snakeDir = [1, 0];
+    snakeDir = snakeDirections.right;
     isDead = false;
 
 }
@@ -75,7 +90,7 @@ function moveSnake() {
         killSnake()
     } else {
         if ((newHead[0] === apple[0] && newHead[1] == apple[1])) {
-            if (snake.length % clueInterval == 0) {cluedClues++}
+            if (snake.length % clueInterval == 0) { cluedClues++ }
             apple = getEmptyCoords();
         } else {
             snake.pop();
@@ -84,26 +99,103 @@ function moveSnake() {
     }
 }
 
+function drawRotated(img, rads, x, y) {
+    ctx.save();
+
+    ctx.translate(x + cellSize / 2, y + cellSize / 2);
+    ctx.rotate(rads);
+    ctx.translate(-x - cellSize / 2, -y - cellSize / 2);
+    ctx.drawImage(img, x, y);
+
+    ctx.restore();
+}
+
+//TODO: This is very very repetetive!
 function drawSnake() {
     ctx.beginPath();
     ctx.fillStyle = isDead ? "#99999" : "#FF0000";
-    snake.forEach(p => {
-        ctx.fillRect(p[0] * cellSize + 1, p[1] * cellSize + 1, cellSize - 2, cellSize - 2);
+    snake.forEach((p, i) => {
+
+        if (i == 0) {
+            // Head
+            if (snakeDir == snakeDirections.right)
+                drawRotated(snakeHead, 0, p[0] * cellSize + 1, p[1] * cellSize + 1);
+            else if (snakeDir === snakeDirections.left)
+                drawRotated(snakeHead, Math.PI, p[0] * cellSize + 1, p[1] * cellSize + 1);
+            else if (snakeDir === snakeDirections.up)
+                drawRotated(snakeHead, -Math.PI / 2, p[0] * cellSize + 1, p[1] * cellSize + 1);
+            else if (snakeDir === snakeDirections.down)
+                drawRotated(snakeHead, Math.PI / 2, p[0] * cellSize + 1, p[1] * cellSize + 1);
+        } else if (i == snake.length - 1) {
+            // Tail
+            let tailDir = [snake[i - 1][0] - p[0], snake[i - 1][1] - p[1]]
+
+            if (arrayEquals(tailDir, snakeDirections.right))
+                drawRotated(snakeTail, 0, p[0] * cellSize + 1, p[1] * cellSize + 1);
+            else if (arrayEquals(tailDir, snakeDirections.left))
+                drawRotated(snakeTail, Math.PI, p[0] * cellSize + 1, p[1] * cellSize + 1);
+            else if (arrayEquals(tailDir, snakeDirections.up))
+                drawRotated(snakeTail, -Math.PI / 2, p[0] * cellSize + 1, p[1] * cellSize + 1);
+            else if (arrayEquals(tailDir, snakeDirections.down))
+                drawRotated(snakeTail, Math.PI / 2, p[0] * cellSize + 1, p[1] * cellSize + 1);
+        } else {
+            // Body or corner
+
+            // Get the direction of the snake
+            let segmentDir = [(snake[i - 1][0] - snake[i + 1][0]) / 2, (snake[i - 1][1] - snake[i + 1][1]) / 2]
+
+            if (arrayEquals(segmentDir, snakeDirections.right))
+                drawRotated(snakeBody, 0, p[0] * cellSize + 1, p[1] * cellSize + 1);
+            else if (arrayEquals(segmentDir, snakeDirections.left))
+                drawRotated(snakeBody, Math.PI, p[0] * cellSize + 1, p[1] * cellSize + 1);
+            else if (arrayEquals(segmentDir, snakeDirections.up))
+                drawRotated(snakeBody, -Math.PI / 2, p[0] * cellSize + 1, p[1] * cellSize + 1);
+            else if (arrayEquals(segmentDir, snakeDirections.down))
+                drawRotated(snakeBody, Math.PI / 2, p[0] * cellSize + 1, p[1] * cellSize + 1);
+            else {
+                // We have a corner
+                let cornerDir1 = [snake[i - 1][0] - p[0], snake[i - 1][1] - p[1]]
+                let cornerDir2 = [p[0] - snake[i + 1][0], p[1] - snake[i + 1][1]]
+
+                if (arrayEquals(cornerDir1, snakeDirections.right))
+                    if (arrayEquals(cornerDir2, snakeDirections.down))
+                        drawRotated(snakeCorner, -Math.PI / 2, p[0] * cellSize + 1, p[1] * cellSize + 1);
+                    else
+                        drawRotated(snakeCorner, 0, p[0] * cellSize + 1, p[1] * cellSize + 1);
+                else if (arrayEquals(cornerDir1, snakeDirections.left))
+                    if (arrayEquals(cornerDir2, snakeDirections.down))
+                        drawRotated(snakeCorner, Math.PI, p[0] * cellSize + 1, p[1] * cellSize + 1);
+                    else
+                        drawRotated(snakeCorner, Math.PI / 2, p[0] * cellSize + 1, p[1] * cellSize + 1);
+                else if (arrayEquals(cornerDir1, snakeDirections.up))
+                    if (arrayEquals(cornerDir2, snakeDirections.left))
+                        drawRotated(snakeCorner, -Math.PI / 2, p[0] * cellSize + 1, p[1] * cellSize + 1);
+                    else
+                        drawRotated(snakeCorner, Math.PI, p[0] * cellSize + 1, p[1] * cellSize + 1);
+                else if (arrayEquals(cornerDir1, snakeDirections.down))
+                    if (arrayEquals(cornerDir2, snakeDirections.left))
+                        drawRotated(snakeCorner, 0, p[0] * cellSize + 1, p[1] * cellSize + 1);
+                    else
+                        drawRotated(snakeCorner, Math.PI / 2, p[0] * cellSize + 1, p[1] * cellSize + 1);
+            }
+
+
+        }
+
+
+
+
+        // ctx.fillRect(p[0] * cellSize + 1, p[1] * cellSize + 1, cellSize - 2, cellSize - 2);
     });
     ctx.closePath();
 }
 
 function drawApple() {
     if (snake.length % clueInterval == 0 && cluedClues < clue.length) {
-        ctx.beginPath();
-        ctx.fillStyle = "#FF00FF";
-        ctx.fillText(clue[cluedClues], apple[0] * cellSize, apple[1] * cellSize);
-        ctx.closePath();
+        ctx.fillStyle = "#FF0000";
+        ctx.fillText(clue[cluedClues], apple[0] * cellSize, (apple[1] + 1) * cellSize);
     } else {
-        ctx.beginPath();
-        ctx.fillStyle = "#FF00FF";
-        ctx.fillRect(apple[0] * cellSize, apple[1] * cellSize, cellSize, cellSize)
-        ctx.closePath();
+        ctx.drawImage(appleImg, apple[0] * cellSize, apple[1] * cellSize);
     }
 }
 
