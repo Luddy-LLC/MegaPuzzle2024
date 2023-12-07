c7caebaa85ec87386b3e39c06585d9d7e6232cbc
mateidragony <matei.cloteaux@gmail.com>
2023-12-07 16:41:33 -0500

    Add Music Puzzle

---
+++ sw/music/script.js
@@ -0,0 +1,29 @@
+const cols = document.getElementsByClassName("col");
+const audios = [new Audio("./assets/audio1.wav"), new Audio("./assets/audio2.wav"), new Audio("./assets/audio3.wav"), 
+                new Audio("./assets/audio4.wav"), new Audio("./assets/audio5.wav"), new Audio("./assets/audio6.wav")]
+
+// blur removes focus
+let noElementFocused = true;
+
+alert("Sound up required for this puzzle");
+
+for (let index = 0; index < cols.length; index++) {
+    const col = cols[index];
+    const audio = audios[index];
+
+    col.addEventListener("click", e => {
+        // if(noElementFocused){
+            for(let i=0; i<cols.length; ++i) cols[i].setAttribute("data-focus", "false");
+            for(let i=0; i<audios.length; ++i) {audios[i].pause(); audios[i].currentTime = 0;}
+
+            audios[index].play();
+            noElementFocused = false;
+            cols[index].setAttribute("data-focus", "true");
+        // }
+    });
+
+    audio.addEventListener("ended", e => {
+        noElementFocused = true;
+        cols[index].setAttribute("data-focus", "false");
+    });
+}
