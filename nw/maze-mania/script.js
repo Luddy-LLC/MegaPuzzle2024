ba5cc30ef3cf905da6e78f97fd15ac90bbd39fc0
Mallory Miller <mallorysilvermiller@gmail.com>
2023-12-21 16:20:26 -0500

    Maze Mania
    TODO: Format better lol, maybe make button less explicit...

---
+++ nw/maze-mania/script.js
@@ -0,0 +1,36 @@
+const layers = ["./assets/MazeSide1.png", "./assets/MazeSide2.png"]
+let maze;
+let button;
+let background;
+
+let top_layer = true;
+
+
+
+  
+window.onload = init;
+
+function init() {
+
+    maze = document.getElementById("maze");
+    button = document.getElementById("swap");
+    background = document.getElementById("background");
+
+}
+
+
+function nextSide() {
+    top_layer = !top_layer;
+    if (top_layer) {
+
+        maze.setAttribute("src", layers[0]);
+        button.setAttribute("top_layer", top_layer);
+        background.setAttribute("top_layer", top_layer);
+    } else {
+        maze.setAttribute("src", layers[1]);
+        button.setAttribute("top_layer", top_layer);
+        background.setAttribute("top_layer", top_layer);
+    }
+}
+
+
