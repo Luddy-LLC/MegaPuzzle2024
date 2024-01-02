7552e6e69fa46517355a11ba4e6fc63760a8e1df
mateidragony <matei.cloteaux@gmail.com>
2024-01-02 16:39:05 -0500

    Update Music Puzzle

--- ithkiya/music/script.js
+++ ithkiya/music/script.js
@@ -1,29 +1,51 @@
 const cols = document.getElementsByClassName("col");
 const audios = [new Audio("./assets/audio1.wav"), new Audio("./assets/audio2.wav"), new Audio("./assets/audio3.wav"), 
                 new Audio("./assets/audio4.wav"), new Audio("./assets/audio5.wav"), new Audio("./assets/audio6.wav")]
+const audioSrcs = ["./assets/audio1.wav", "./assets/audio2.wav", "./assets/audio3.wav", 
+"./assets/audio4.wav", "./assets/audio5.wav", "./assets/audio6.wav"]
+
+const audioElement = document.getElementById("current-audio");
 
 // blur removes focus
-let noElementFocused = true;
 
-alert("Sound up required for this puzzle");
+let soundCD = 0;
+
+window.addEventListener("click", e => {
+    if(soundCD <= 0){
+        for (let index = 0; index < cols.length; index++) {
+            const col = cols[index];
+            const audio = audios[index];
+
+            audioElement.pause();
+            
+            audioElement.style.visibility = "hidden";
+            col.setAttribute("data-focus", "false");
+        }
+    }
+
+    soundCD = 0;
+});
 
 for (let index = 0; index < cols.length; index++) {
     const col = cols[index];
     const audio = audios[index];
 
     col.addEventListener("click", e => {
-        // if(noElementFocused){
-            for(let i=0; i<cols.length; ++i) cols[i].setAttribute("data-focus", "false");
-            for(let i=0; i<audios.length; ++i) {audios[i].pause(); audios[i].currentTime = 0;}
+        for(let i=0; i<cols.length; ++i) cols[i].setAttribute("data-focus", "false");
+        for(let i=0; i<audios.length; ++i) {audios[i].pause(); audios[i].currentTime = 0;}
 
-            audios[index].play();
-            noElementFocused = false;
-            cols[index].setAttribute("data-focus", "true");
-        // }
+
+        audioElement.src = audioSrcs[index];
+        audioElement.style.visibility = "visible";
+        audioElement.play();
+        noElementFocused = false;
+        col.setAttribute("data-focus", "true");
+
+        soundCD = 10;
     });
 
     audio.addEventListener("ended", e => {
-        noElementFocused = true;
-        cols[index].setAttribute("data-focus", "false");
+        col.setAttribute("data-focus", "false");
+        soundCD = 0;
     });
 }
