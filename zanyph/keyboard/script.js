16b5e5767e8a0ae1a918fb47eb4f20254349fa71
NouveauReece <nouveaureece@protonmail.com>
2024-01-06 17:06:08 -0500

    ⌨️ Added Reece's silly little keyboard layouts :)

--- zanyph/keyboard/script.js
+++ zanyph/keyboard/script.js
@@ -8,11 +8,17 @@ for(let i=1; i<=7; ++i){
 }
 
 const qwertyLayout = ['e', 'd', 'u', '-', 'h', 'z', '=', 'p', ';', ']', '/', '[', 'l', '8', 'w', 's', '5', '9', 'o', '.', '6', 'v', '3', '`', 'g', 'j', 'q', '1', 't', 'y', "'", '\\', '\\', 'k', 'f', 'i', 'r', 'x', 'a', '2', '7', 'm', '4', '0', 'n', 'b', 'c', ','];
+const qwertyLayoutRomaji = ['e', 'd', 'u', '-', 'h', 'z', '=', 'p', ';', ']', '/', '[', 'l', '8', 'w', 's', '5', '9', 'o', '.', '6', 'v', '3', '`', 'g', 'j', 'q', '1', 't', 'y', "'", '§', '\\', 'k', 'f', 'i', 'r', 'x', 'a', '2', '7', 'm', '4', '0', 'n', 'b', 'c', ','];
 const azertyLayout = ['e', 'd', 'u', ')', 'h', 'w', '=', 'p', 'm', '$', '!', '^', 'l', '_', 'z', 's', '(', 'ç', 'o', ':', '-', 'v', '"', '²', 'g', 'j', 'a', '&', 't', 'y', 'ù', '<', '*', 'k', 'f', 'i', 'r', 'x', 'q', 'é', 'è', ',', "'", 'à', 'n', 'b', 'c', ';'];
 const qwertyLayoutUK = ['e', 'd', 'u', '-', 'h', 'z', '=', 'p', ';', ']', '/', '[', 'l', '8', 'w', 's', '5', '9', 'o', '.', '6', 'v', '3', '`', 'g', 'j', 'q', '1', 't', 'y', "'", '\\', '#', 'k', 'f', 'i', 'r', 'x', 'a', '2', '7', 'm', '4', '0', 'n', 'b', 'c', ','];
 const qwertyLayoutGerman = ['e', 'd', 'u', 'ß', 'h', 'y', '´', 'p', 'ö', '+', '-', 'ü', 'l', '8', 'w', 's', '5', '9', 'o', '.', '6', 'v', '3', '^', 'g', 'j', 'q', '1', 't', 'z', 'ä', '<', '#', 'k', 'f', 'i', 'r', 'x', 'a', '2', '7', 'm', '4', '0', 'n', 'b', 'c', ','];
 const qwertyLayoutLatin = ['e', 'd', 'u', "'", 'h', 'z', '¿', 'p', 'ñ', '+', '-', '´', 'l', '8', 'w', 's', '5', '9', 'o', '.', '6', 'v', '3', '|', 'g', 'j', 'q', '1', 't', 'y', '{', '<', '}', 'k', 'f', 'i', 'r', 'x', 'a', '2', '7', 'm', '4', '0', 'n', 'b', 'c', ','];
 const qwertyLayoutSpanish = ['e', 'd', 'u', "'", 'h', 'z', '¡', 'p', 'ñ', '+', '-', '`', 'l', '8', 'w', 's', '5', '9', 'o', '.', '6', 'v', '3', 'º', 'g', 'j', 'q', '1', 't', 'y', '´', '<', 'ç', 'k', 'f', 'i', 'r', 'x', 'a', '2', '7', 'm', '4', '0', 'n', 'b', 'c', ','];
+const qwertyLayoutSpanishLegacy = ['e', 'd', 'u', '-', 'h', 'z', '=', 'p', 'ñ', '`', 'ç', '´', 'l', '8', 'w', 's', '5', '9', 'o', '.', '6', 'v', '3', '<', 'g', 'j', 'q', '1', 't', 'y', ';', '[', "'", 'k', 'f', 'i', 'r', 'x', 'a', '2', '7', 'm', '4', '[', '0', 'n', 'b', '<', 'c', ','];
+const qwertyLayoutGermanQWERTZMac = ['e', 'd', 'u', 'ß', 'h', 'y', '´', 'p', 'ö', '+', '-', 'ü', 'l', '8', 'w', 's', '5', '9', 'o', '.', '6', 'v', '3', '<', 'g', 'j', 'q', '1', 't', 'z', 'ä', '^', '#', 'k', 'f', 'i', 'r', 'x', 'a', '2', '7', 'm', '4', '^', '0', 'n', 'b', '<', 'c', ','];
+
+// ¡!#$%/&*()
+
 
 function iteratorEqualsArray(arr, iter){
 
@@ -29,20 +35,37 @@ function iteratorEqualsArray(arr, iter){
 if ('keyboard' in navigator && 'lock' in navigator.keyboard) { // Supported!
     const keyboard = navigator.keyboard;
 
+
+
+
     window.onload = () => {
         keyboard.getLayoutMap().then((keyboardLayoutMap) => {
+
+            console.log(qwertyLayoutGerman);
+
+            let temp = [];
+
+            for(const item of keyboardLayoutMap.values()){
+                temp.push(item);
+            }
+
+            console.log(temp);
+
             /*
+
+
 
             US/India/Japanese/Korean: !@#$%^&*()
             France: &é"'(-è_çà
             Germany: !"§$%&/()=
+            GermanyQWERTZMac !@#$%^&*()
             UK: !"£$%^&*()
             Latin: !"#$%&/()=
             Spanish: !"·$%&/()=
 
             */
 
-            if(iteratorEqualsArray(qwertyLayout, keyboardLayoutMap.values())){ // US/India/Japanese/Korean QWERTY
+            if(iteratorEqualsArray(qwertyLayout, keyboardLayoutMap.values()) || iteratorEqualsArray(qwertyLayoutRomaji, keyboardLayoutMap.values())){ // US/India/Japanese/Korean QWERTY
                 /*
                 !% + @& = #$
                 $ + @ = #
