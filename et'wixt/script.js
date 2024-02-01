4c07fd8ef5d0478c519b336d6d09ff051851ab0a
NouveauReece <nouveaureece@protonmail.com>
2024-02-01 15:02:57 -0500

    🌥️ Clouds in Progress!!

---
+++ et'wixt/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('body').style.opacity = 1;
+    // document.querySelector('body').style.background = "#E3DED4";
+    document.getElementsByClassName('clouds')[0].style.opacity = 0;
+    document.getElementsByClassName('region-map')[0].style.height = "100%";
+    document.getElementsByClassName('cloud-background')[0].style.height = "100%";
+}) 
