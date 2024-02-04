806cc1f8e0f7f477fd4b6ad7f3b4003c2476b09d
mateidragony <matei.cloteaux@gmail.com>
2024-02-04 15:19:25 -0500

    🗺️ Fix submaps

--- ithkiya/music/script.js
+++ ithkiya/music/script.js
@@ -6,13 +6,28 @@ const audioSrcs = ["./assets/audio1.wav", "./assets/audio2.wav", "./assets/audio
 
 const audioElement = document.getElementById("current-audio");
 
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
 
 // blur removes focus
 
 let soundCD = 0;
 
-window.addEventListener("click", e => {
+function playSound(index, col) {
+    for(let i=0; i<cols.length; ++i) cols[i].setAttribute("data-focus", "false");
+    for(let i=0; i<audios.length; ++i) {audios[i].pause(); audios[i].currentTime = 0;}
+
+
+    audioElement.src = audioSrcs[index];
+    audioElement.style.visibility = "visible";
+    audioElement.play();
+    noElementFocused = false;
+    col.setAttribute("data-focus", "true");
+
+    soundCD = 10;
+}
+
+function stopSound(){
     if(soundCD <= 0){
         for (let index = 0; index < cols.length; index++) {
             const col = cols[index];
@@ -26,25 +41,34 @@ window.addEventListener("click", e => {
     }
 
     soundCD = 0;
-});
+}
+
+if(!isMobile) document.addEventListener("click", e => stopSound());
+
+window.onkeydown = document.onkeydown = e => {
+    let keyCode = e.key || e.code;
+    if (keyCode == 'Escape') stopSound();
+}
+
+
 
 for (let index = 0; index < cols.length; index++) {
     const col = cols[index];
     const audio = audios[index];
 
-    col.addEventListener("click", e => {
-        for(let i=0; i<cols.length; ++i) cols[i].setAttribute("data-focus", "false");
-        for(let i=0; i<audios.length; ++i) {audios[i].pause(); audios[i].currentTime = 0;}
+    col.addEventListener("click", e => playSound(index, col));
 
+    col.onkeypress = e => {
+        let keyCode = e.code || e.key;
+        if (keyCode == 'Enter' && document.activeElement == col){
+            playSound(index, col);
+        }
+    }
 
-        audioElement.src = audioSrcs[index];
-        audioElement.style.visibility = "visible";
-        audioElement.play();
-        noElementFocused = false;
-        col.setAttribute("data-focus", "true");
-
-        soundCD = 10;
-    });
+    col.onkeydown = e => {
+        let keyCode = e.key || e.code;
+        if (keyCode == 'Escape') stopSound();
+    }
 
     audio.addEventListener("ended", e => {
         col.setAttribute("data-focus", "false");
--- ithkiya/script.js
+++ ithkiya/script.js
@@ -1,2 +1,8 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('body').style.opacity = 1;
+    document.getElementsByClassName('clouds')[0].style.opacity = 0;
+    document.getElementsByClassName('region-map')[0].style.height = "100%";
+    document.getElementsByClassName('cloud-background')[0].style.height = "100%";
+}) 
