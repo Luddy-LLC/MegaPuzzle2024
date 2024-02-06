ad27d8e14733a1aa1136e11bc2a0d411b4b22465
mateidragony <matei.cloteaux@gmail.com>
2024-02-06 16:45:11 -0500

    ☁️ Add cloud transitions

--- et'wixt/script.js
+++ et'wixt/script.js
@@ -1,10 +1,21 @@
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-document.addEventListener('DOMContentLoaded', function() {
+
+window.onload = () => {
     document.querySelector('body').style.opacity = 1;
-    // document.querySelector('body').style.background = "#E3DED4";
+    document.getElementsByClassName('clouds')[0].src = '/map/assets/fast_clouds_pixleated.webp';
     document.getElementsByClassName('clouds')[0].style.opacity = 0;
     document.getElementsByClassName('region-map')[0].style.height = "100%";
-    document.getElementsByClassName('cloud-background')[0].style.height = "100%";
-}) 
+}
+
+window.transitionToPage = function(href) {
+    document.getElementById('clouds').style.opacity = 1;
+    document.getElementById('clouds').src = '/map/assets/clouds_pixleated_reverse_fast.webp';
+    setTimeout(function() {
+        document.querySelector('body').style.opacity = 0;
+        setTimeout(function() { 
+            window.location.href = href
+        }, 500)
+    }, 1000) // timeout within timeout!!!!!!   
+}
