c1ecaec2518ea4869d6f82fa3f1926cfafecf8b6
Calvin Josenhans <60663079+HalflingHelper@users.noreply.github.com>
2024-02-24 19:21:44 -0500

    👽bogos binted?

--- aavikk/maze-mania/script.js
+++ aavikk/maze-mania/script.js
@@ -5,32 +5,27 @@ let background;
 
 let top_layer = true;
 
-
-
-  
 window.onload = init;
 
 function init() {
 
     maze = document.getElementById("maze");
     button = document.getElementById("swap");
-    background = document.getElementsByTagName('main')[0];
-
+    // background = document.getElementsByTagName('main')[0];
 }
 
 
 function nextSide() {
     top_layer = !top_layer;
     if (top_layer) {
-
         maze.setAttribute("src", layers[0]);
         button.setAttribute("top_layer", top_layer);
-        background.setAttribute("top_layer", top_layer);
+        document.body.style.backgroundImage = "url(./assets/MazeSide1-bg.png)";
+        
     } else {
         maze.setAttribute("src", layers[1]);
         button.setAttribute("top_layer", top_layer);
-        background.setAttribute("top_layer", top_layer);
+        document.body.style.backgroundImage = "none";
+        document.body.style.backgroundColor = "rgb(211,184,144)";
     }
 }
-
-
