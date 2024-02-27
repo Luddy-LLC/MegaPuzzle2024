2064effce4d1139f9d8c335da0d7cb2386529729
Mallory Miller <mallorysilvermiller@gmail.com>
2024-02-27 10:48:31 -0500

    Ouroboros new hint fixed

--- ithkiya/ouroboros/script.js
+++ ithkiya/ouroboros/script.js
@@ -1,12 +1,12 @@
 function switchCypher() {
     const text = document.getElementById('cypher');
     const btn = document.getElementById('reveal');
-    if (text.innerText.includes('Aovbno')) {
-        text.innerText = "Though the Lake of Lerna may be empty, this Cube of Creation fills aplenty."
+    if (text.innerText.includes('Dpao')) {
+        text.innerText = "With spindly hand a thin line it does tread, weaving a quite different kind of thread."
         btn.classList.remove('btn-primary');
         btn.classList.add('btn-secondary');
     } else {
-        text.innerText = "Aovbno aol Shrl vm Slyuh thf il ltwaf, aopz Jbil vm Jylhapvu mpssz hwsluaf."
+        text.innerText = "Dpao zwpuksf ohuk h aopu spul pa kvlz aylhk, dlhcpun h xbpal kpmmlylua rpuk vm aoylhk. "
         btn.classList.remove('btn-secondary');
         btn.classList.add('btn-primary');
     }
