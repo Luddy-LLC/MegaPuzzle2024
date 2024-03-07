d3d0e602dc4f762dd13c783515522e9e5667405c
Matei Cloteaux <matei.cloteaux@gmail.com>
2024-03-07 23:12:02 +0000

    Workflowtest

--- leaderboard/data.js
+++ leaderboard/data.js
@@ -1,1 +1,1 @@
-let data = "[{Name:Ronak,Num:1},{Name:Calvin,Num:2},{Name:John,Num:3},{Name:Singh,Num:4},{Name:Spoosh,Num:5},{Name:Calcin,Num:10},{Name:Matei ,Num:99},{Name:Calcite,Num:23}]"
+let data = "[{Name:Ronak,Num:1},{Name:Calvin,Num:2},{Name:John,Num:3},{Name:Singh,Num:4},{Name:Spoosh,Num:5},{Name:Calcin,Num:100},{Name:Matei ,Num:9999},{Name:Calcite,Num:234}]"
