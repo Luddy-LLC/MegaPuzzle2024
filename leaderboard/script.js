42b42a731430c89e8dc3423dd97fc156aecfea90
Matei Cloteaux <matei.cloteaux@gmail.com>
2024-03-07 18:08:57 -0500

    Date?

--- leaderboard/script.js
+++ leaderboard/script.js
@@ -3,23 +3,30 @@ onscroll = e => {
     mall.style.backgroundPositionY = `${-window.scrollY/5}px`;
 }
 
-// const tbody = document.getElementById("tbody");
+const update = document.getElementById("update");
+update.innerText = "Last updated: "+date; 
 
-// data = data.substring(2, data.length-2);
-// let newData = data.split("},{");
-// for(let i=0; i<newData.length; ++i){
-//     let newNewData = newData[i].substring(5);
-//     let newNewNewData = newNewData.split(",Num:");
+const tbody = document.getElementById("tbody");
 
-//     let tr = document.createElement("tr");
-//     let name = document.createElement("td");
-//     let score = document.createElement("td");
+data = data.substring(2, data.length-2);
+let newData = data.split("},{");
+for(let i=0; i<newData.length; ++i){
+    let newNewData = newData[i].substring(5);
+    let newNewNewData = newNewData.split(",Num:");
 
-//     name.innerText = newNewNewData[0];
-//     score.innerText = newNewNewData[1];
+    let tc = document.createElement("div");
+    tc.classList.add("text-container");
 
-//     tr.appendChild(name);
-//     tr.appendChild(score);
+    let name = document.createElement("div");
+    let score = document.createElement("div");
+    name.classList.add("td");
+    score.classList.add("td");
 
-//     tbody.appendChild(tr);
-// }
+    name.innerText = newNewNewData[0];
+    score.innerText = newNewNewData[1];
+
+    tc.appendChild(name);
+    tc.appendChild(score);
+
+    tbody.appendChild(tc);
+}
