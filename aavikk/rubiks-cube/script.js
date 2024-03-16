905a067ae966d4e7df11d10575340002fa5cf751
Matei Cloteaux <matei.cloteaux@gmail.com>
2024-03-16 14:45:48 -0400

    ⛩️ Hints!!

---
+++ aavikk/rubiks-cube/script.js
@@ -0,0 +1,12 @@
+const one = document.getElementById("i001");
+one.onmouseenter = e => one.innerText = "Inscryption #001"
+one.onmouseleave = e => one.innerText = "Inscription #001"
+
+
+const two = document.getElementById("i002");
+two.onmouseenter = e => two.innerText = "Inscryption #002"
+two.onmouseleave = e => two.innerText = "Inscription #002"
+
+const insc = document.getElementById("insc");
+insc.onmouseenter = e => insc.innerText = "inscryptions"
+insc.onmouseleave = e => insc.innerText = "inscriptions"
